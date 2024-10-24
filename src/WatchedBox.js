382dd18521f3eb60b1c77927399fcad02a5df10a
Dominik Speidel <dominik-speidel@posteo.de>
2024-10-24 09:12:47 +0200

    initial commit

---
+++ src/WatchedBox.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import WatchedSummary from "./WatchedSummary";
+import WatchedMovieList from "./WatchMovieList";
+
+export default function WatchedBox({ tempWatchedData }) {
+  const [isOpen2, setIsOpen2] = useState(true);
+  const [watched, setWatched] = useState(tempWatchedData);
+
+  return (
+    <div className="box">
+      <button
+        className="btn-toggle"
+        onClick={() => setIsOpen2((open) => !open)}
+      >
+        {isOpen2 ? "–" : "+"}
+      </button>
+      {isOpen2 && (
+        <>
+          <WatchedSummary watched={watched} />
+          <WatchedMovieList watched={watched} />
+        </>
+      )}
+    </div>
+  );
+}
